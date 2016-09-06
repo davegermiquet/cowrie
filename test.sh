eab104a01169d14a5c7bd654578823874eeaab13
Dave Germiquet <davegermiquet@trulycanadian.net>
2016-09-05 20:46:22 -0400

    TEST

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
-PYTHONPATH=${TRAVIS_BUILD_DIR}
+PYTHONPATH=${TRAVIS_BUILD_DIR}:${TRAVIS_BUILD_DIR}/cowrie/core
 export PYTHONPATH
+python 
 cd ${TRAVIS_BUILD_DIR}
+python test.pl
 python $VIRTUAL_ENV/bin/trial cowrie
-
